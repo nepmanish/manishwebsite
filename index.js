269f82e1d86065dee08cf03414db4716b33c0b3e
nepmanish <138556950+nepmanish@users.noreply.github.com>
2023-07-04 11:48:08 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+let menuBtn = document.querySelector('#menu-btn');
+let menuBar = document.querySelector('.menu-bar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('bx-x');
+    menuBar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('bx-x');
+    menuBar.classList.remove('active');
+}
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-bio h1,.about-resume', {origin: 'left'});
+ScrollReveal().reveal('.home-bio p', {origin: 'right'});
+ScrollReveal().reveal('.home-bio, heading', {origin: 'top'});
+ScrollReveal().reveal('.profile-pic, .about-items, .skills-items, .projects-items, .form', {origin: 'bottom'});
+
+
+const animText = new Typed ('.animated-text',{
+    strings: ['Web Designer','Freelancer','Gamer'],
+    backSpeed: 100,
+    typeSpeed: 100,
+    backDelay: 600,
+    loop: true
+});
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_uohldbf";
+    const templateID = "template_07c4vi2";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
